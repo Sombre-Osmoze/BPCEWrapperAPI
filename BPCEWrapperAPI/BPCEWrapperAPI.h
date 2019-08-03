475d7a2eb9efa98971e4e819f759497610324980
Marcus Florentin <fmarcus28@gmx.fr>
2019-08-03 15:48:16 +0200

    Initial Commit

---
+++ BPCEWrapperAPI/BPCEWrapperAPI.h
@@ -0,0 +1,19 @@
+//
+//  BPCEWrapperAPI.h
+//  BPCEWrapperAPI
+//
+//  Created by Marcus Florentin on 03/08/2019.
+//  Copyright © 2019 Marcus Florentin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BPCEWrapperAPI.
+FOUNDATION_EXPORT double BPCEWrapperAPIVersionNumber;
+
+//! Project version string for BPCEWrapperAPI.
+FOUNDATION_EXPORT const unsigned char BPCEWrapperAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BPCEWrapperAPI/PublicHeader.h>
+
+
